567d8b6602769c76cbc70de590fd4dc8ded7373a
zidder <zidder@hilearn.io>
2018-10-05 19:50:30 +0400

    quantities as numbers
    Summary: 	returning quantities as numbers instead of strings in response
    
    Test Plan: 	none
    
    Reviewers: arsen, davkhech
    
    Reviewed By: arsen, davkhech
    
    Differential Revision: http://phabricator.hilearn.io/D1552

--- webserver/views.py
+++ webserver/views.py
@@ -15,6 +15,7 @@ from webserver.utils import get_portfolio
 
 
 class HealthCkeckView(APIView):
+
     def get(self, request):
         return Response({"status": "ok"})
 
@@ -31,13 +32,11 @@ class PortfolioView(APIView):
     @with_valid_api_key
     @initialize_exchange
     def put(self, request, exchange, params):
-
         allocations = params['allocations']
         total_weight = sum(Decimal(allocation['portion'])
                            for allocation in allocations)
         if total_weight > 1:
             raise WeightsSumGreaterThanOne
-
         found_btc = False
         allocations = [{k: (v if k != "portion" else Decimal(v))
                         for k, v in allocation.items()}
@@ -58,7 +57,6 @@ class PortfolioView(APIView):
         result = tasks.rebalance_task.delay(request.data,
                                             request.user.api_key,
                                             weights)
-
         return Response({
             "status": "target allocations queued for processing",
             "portfolio_processing_request":
@@ -73,11 +71,9 @@ class ProcessingView(APIView):
     @with_valid_api_key
     def post(self, request, process_id):
         result = AsyncResult(process_id, app=tasks.app)
-
         if (result.state == "PENDING" or
                 result.result["api_key"] != request.user.api_key):
             raise NotFound("not found or expired")
-
         if result.status == "STARTED":
             return Response({
                 "status": "processing in progress",
@@ -85,9 +81,21 @@ class ProcessingView(APIView):
                     "/api/portfolio_process/{}".format(result.id),
                 "retry_after": result.result['remaining_time_estimate']
             })
-
         response = result.result
         response.pop('api_key')
+        for market in response:
+            if market != 'status':
+                break
+        response[market]['value'] = float(response[market]['value'])
+        response[market]['allocations'] = [
+            {
+                'coin': d['coin'],
+                'amount': float(d['amount']),
+                'portion': float(d['portion'])
+            }
+            for d in response[market]['allocations']
+        ]
+
         return Response(response)
 
 
