b3213d3a0a8859581f2156b34886753d4952e7c3
Tom Counsell <other@tomcounsell.com>
2018-10-27 18:12:19 +0200

    reset secret key

--- webserver/settings.py
+++ webserver/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@&cre$@dl)filxt9fbibp7)b^vvyr$gtf44%hrnmoq0fv#w7sq'
+SECRET_KEY = os.environ.get('SECRET_KEY', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
